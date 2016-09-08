b66fdb0064e25f387fe45d07a35220f82bcaa54c
David Yarnell <dyarnell@epa.gov>
2016-09-08 11:21:59 -0700

    Remove all streetview widgets from repository. More information about why these widgets were remove can be found at https://geonet.esri.com/docs/DOC-2991-google-street-view-widget-21-072016

--- widgets/StreetView/_build-generate_module.js
+++
@@ -1,4 +0,0 @@
-define(["dojo/text!./Widget.html",
-"dojo/text!./css/style.css",
-"dojo/i18n!./nls/strings",
-"dojo/text!configs/StreetView/config_Google Street View.json"], function(){});
--- widgets/StreetViewPopup/nls/zh-cn/strings.js
+++
@@ -1,4 +0,0 @@
-﻿define({
-    label1: "试_I am a demo widget._验",
-    label2: "试_This is configurable._验"
-});
